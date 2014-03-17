5d96b2d2a0484eba7569f2d8e01d231408dd61a3
tasukunakano <b1010243@fun.ac.jp>
2014-03-17 09:58:20 +0900

    ルートを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MutenetCrm::Application.routes.draw do
   resources :companies
 
+  root 'companies#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

548d830457db80737e259a33fc4b8dfccf084120
tasukunakano <b1010243@fun.ac.jp>
2014-03-19 09:20:51 +0900

    バリデーションを追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,10 @@
+require 'nkf'
+
 class Company < ActiveRecord::Base
-validates :company, presence: true
+	validates :company, presence: true
+	validates :kana, format:{ with:/\A[\p{Katakana}\u30fc]+\z/}
+
+	before_validation do
+		self.kana = NKF.nkf('-wh2', kana) if kana
+	end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -13,4 +13,20 @@ describe Company do
   	expect(company).not_to be_valid
   	expect(company.errors[:company]).to be_present
   end
+
+  specify "kanaは漢字を含まない" do
+  	company[:kana] = '株式会社'
+  	expect(company).not_to be_valid
+  	expect(company.errors[:kana]).to be_present
+  end
+
+  specify "kanaはカタカナのみ受け入れる" do
+  	expect(company).to be_valid
+  end
+
+  specify "kanaに含まれるひらがなはカタカナに変換して受け入れる" do
+  	company[:kana] = 'みゅーとねっと'
+  	expect(company).to be_valid
+  	expect(company[:kana]).to eq('ミュートネット')
+  end
 end
